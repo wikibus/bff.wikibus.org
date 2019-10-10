c36d09320b938202a23db5342f60379aec063e70
tpluscode <tpluscode@users.noreply.github.com>
2019-10-10 10:13:57 +0200

    feat: first complere

--- handlers/entrypoint.ts
+++ handlers/entrypoint.ts
@@ -1,6 +1,15 @@
-import { NextFunction, Request, Response } from 'express'
+import { HydraBoxRequest, HydraBoxResponse } from 'express'
+import $rdf from 'rdf-ext'
+import cf from 'clownface'
+import { api, hydra, rdf } from '../lib/ns'
 
-export function get (req: Request, res: Response, next: NextFunction) {
-  res.status(200)
-  next()
+export function get (req: HydraBoxRequest, res: HydraBoxResponse) {
+  const graph = cf($rdf.dataset(), process.env.BASE_URI)
+
+  graph.addOut(rdf.type, api.Entrypoint)
+  graph.addOut(api.Library, process.env.API_LIBRARY, libraryLink => {
+    libraryLink.addOut(hydra.title, 'Collection of physical media about public transport')
+  })
+
+  res.graph(graph.dataset)
 }
---
+++ lib/ns.ts
@@ -0,0 +1,6 @@
+import namespace from '@rdfjs/namespace'
+import { prefixes } from '@zazuko/rdf-vocabularies'
+
+export const rdf = namespace(prefixes.rdf)
+export const hydra = namespace(prefixes.hydra)
+export const api = namespace('https://api.wikibus.org/bff/')
