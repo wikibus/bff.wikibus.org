1dbf2be611d2301f698ed3ba23263ce889d3682f
tpluscode <tpluscode@users.noreply.github.com>
2019-10-11 13:03:42 +0200

    style: whatever happened to this file

--- index.ts
+++ index.ts
@@ -1,38 +1,38 @@
-import cors from "cors";
-import path from "path";
-import hydraBox from "hydra-box";
-import url from "url";
-import express from "express";
-import dotenv from "dotenv";
-import dotenvExpand from "dotenv-expand";
+import cors from 'cors'
+import path from 'path'
+import hydraBox from 'hydra-box'
+import url from 'url'
+import express from 'express'
+import dotenv from 'dotenv'
+import dotenvExpand from 'dotenv-expand'
 
-dotenvExpand(dotenv.config());
+dotenvExpand(dotenv.config())
 
-function hydraMiddleware() {
+function hydraMiddleware () {
   return hydraBox.fromUrl(
-    "/api",
-    "file://" + path.join(__dirname, "hydra/api.ttl"),
+    '/api',
+    'file://' + path.join(__dirname, 'hydra/api.ttl'),
     {
-      contextHeader: "/context/"
+      contextHeader: '/context/',
     }
-  );
+  )
 }
 
 Promise.resolve()
   .then(async () => {
-    const baseUrl = `${process.env.BASE_URI}`;
+    const baseUrl = `${process.env.BASE_URI}`
 
-    const app = express();
+    const app = express()
 
     app.use(
       cors({
-        exposedHeaders: ["link", "location"]
+        exposedHeaders: ['link', 'location'],
       })
-    );
-    app.use(await hydraMiddleware());
+    )
+    app.use(await hydraMiddleware())
 
     app.listen(new url.URL(baseUrl).port, () => {
-      console.log(`listening at ${baseUrl}`);
-    });
+      console.log(`listening at ${baseUrl}`)
+    })
   })
-  .catch(err => console.error(err));
+  .catch(err => console.error(err))
