7433f7a59de48f8ff8b40644df34c430071c934f
tpluscode <tpluscode@users.noreply.github.com>
2019-10-11 17:14:31 +0200

    fix: link shoule be lower case

--- handlers/entrypoint.ts
+++ handlers/entrypoint.ts
@@ -7,7 +7,7 @@ export function get (req: HydraBoxRequest, res: HydraBoxResponse) {
   const graph = cf($rdf.dataset(), process.env.BASE_URI)
 
   graph.addOut(rdf.type, api.Entrypoint)
-  graph.addOut(api.Library, $rdf.namedNode(process.env.API_LIBRARY), libraryLink => {
+  graph.addOut(api.library, $rdf.namedNode(process.env.API_LIBRARY), libraryLink => {
     libraryLink.addOut(hydra.title, 'Collection of physical media about public transport')
   })
 
