5aec1451f6f3219a38da3bea0140d991d4fa60f0
tpluscode <tpluscode@users.noreply.github.com>
2019-10-13 20:53:26 +0200

    fix: add missing metadata

--- handlers/entrypoint.ts
+++ handlers/entrypoint.ts
@@ -6,7 +6,9 @@ import { api, hydra, rdf } from '../lib/ns'
 export function get (req: HydraBoxRequest, res: HydraBoxResponse) {
   const graph = cf($rdf.dataset(), process.env.BASE_URI)
 
-  graph.addOut(rdf.type, api.Entrypoint)
+  graph.addOut(rdf.type, api.EntryPoint)
+  graph.addOut(hydra.title, 'wikibus.org')
+  graph.addOut(hydra.description, 'Online public transport encyclopedia')
   graph.addOut(api.library, $rdf.namedNode(process.env.API_LIBRARY), libraryLink => {
     libraryLink.addOut(hydra.title, 'Collection of physical media about public transport')
   })
--- lib/ns.ts
+++ lib/ns.ts
@@ -3,4 +3,4 @@ import { prefixes } from '@zazuko/rdf-vocabularies'
 
 export const rdf = namespace(prefixes.rdf)
 export const hydra = namespace(prefixes.hydra)
-export const api = namespace('https://api.wikibus.org/bff/')
+export const api = namespace('https://wikibus.org/api#')
