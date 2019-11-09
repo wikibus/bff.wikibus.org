166fb95a36ce4c2d0eefe1023223da818cfc1a3f
tpluscode <tpluscode@users.noreply.github.com>
2019-11-09 11:36:35 +0100

    feat: added data sheet entrypoint link

--- handlers/entrypoint.ts
+++ handlers/entrypoint.ts
@@ -10,8 +10,16 @@ export function get (req: HydraBoxRequest, res: HydraBoxResponse) {
   graph.addOut(hydra.title, 'wikibus.org')
   graph.addOut(hydra.description, 'Online public transport encyclopedia')
   graph.addOut(api.library, $rdf.namedNode(process.env.API_LIBRARY), libraryLink => {
-    libraryLink.addOut(hydra.title, 'Collection of physical media about public transport')
+    libraryLink.addOut(hydra.title, 'Library')
+    libraryLink.addOut(hydra.description, 'Collection of physical media about public transport')
   })
+
+  if (process.env.DATA_SHEETS) {
+    graph.addOut(api.dataSheets, $rdf.namedNode(process.env.DATA_SHEETS), link => {
+      link.addOut(hydra.title, 'Data sheets')
+      link.addOut(hydra.description, 'Technical data gathered from brochures, books, etc')
+    })
+  }
 
   res.graph(graph.dataset)
 }
